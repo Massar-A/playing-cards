38b325c574df859b1b5fc04424c1313e555a66a0
Massar <massarabbas62@gmail.com>
2024-09-18 13:44:26 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from "./components/playing-card/playing-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [PlayingCardComponent]
 })
 export class AppComponent {
-  title = 'playing-cards';
 }
